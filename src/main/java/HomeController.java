7eab63446d98dbeb4163af1d4efb47ccc8fdd704
leeminhyuk93 <dlalsgur1003@naver.com>
2023-08-07 20:23:03 +0900

    새로운 업데이트

--- src/main/java/HomeController.java
+++
@@ -1,5 +0,0 @@
-
-
-public class HomeController {
-    private String mainString;
-}
---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,40 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+    // 실제 데이터베이스에 쿼리를 날리는 부분이 없다. 가능한 이유는 JPA의 '영속성 컨텍스트' 때문이다.
+
+}
